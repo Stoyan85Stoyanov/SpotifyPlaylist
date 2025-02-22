d986b7c33a28802516882ee3cef14e60211fa00f
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-02-22 12:13:44 +0200

    Spotify Playlist App

---
+++ src/main/java/bg/softuni/spotifyplaylist/repository/SongRepository.java
@@ -0,0 +1,22 @@
+package bg.softuni.spotifyplaylist.repository;
+
+import bg.softuni.spotifyplaylist.entity.Song;
+import bg.softuni.spotifyplaylist.entity.Style;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface SongRepository extends JpaRepository<Song, UUID> {
+
+    List<Song> findSongByStyle(Style style);
+
+    @Query("SELECT s FROM Song s")
+    List<Song> findAllSongs();
+
+//    List<Song> findByStyle(Style style);
+
+}
