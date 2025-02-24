cd5327dd8314c9d5640b0e39d39868725b41ce86
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-02-24 20:06:49 +0200

    Spotify Playlist App

---
+++ src/main/java/bg/softuni/spotifyplaylist/entity/User.java
@@ -0,0 +1,60 @@
+package bg.softuni.spotifyplaylist.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity{
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER )
+    private Set<Song> playlist;
+
+    public User() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Song> getPlaylist() {
+        return playlist;
+    }
+    public void setPlaylist(Set<Song> playlist) {
+        this.playlist = playlist;
+    }
+    public void addMyPlaylist(Song song) {
+        this.playlist.add(song);
+    }
+}
