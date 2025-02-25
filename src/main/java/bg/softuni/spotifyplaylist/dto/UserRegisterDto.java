ca0cc2b1313e9e206731d8639a8f436c4a439984
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-02-25 19:26:29 +0200

    Spotify Playlist App

---
+++ src/main/java/bg/softuni/spotifyplaylist/dto/UserRegisterDto.java
@@ -0,0 +1,59 @@
+package bg.softuni.spotifyplaylist.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class UserRegisterDto {
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String password;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 3, max = 20)
+    private String confirmPassword;
+
+    public UserRegisterDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+}
