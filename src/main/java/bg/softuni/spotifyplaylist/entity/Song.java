87de000538ff52f22026959dca15d5427995103e
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-20 18:24:29 +0200

    Spotify Playlist App

--- src/main/java/bg/softuni/spotifyplaylist/entity/Song.java
+++ src/main/java/bg/softuni/spotifyplaylist/entity/Song.java
@@ -1,10 +1,16 @@
 package bg.softuni.spotifyplaylist.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "songs")
 public class Song extends BaseEntity{
 
@@ -13,6 +19,7 @@ public class Song extends BaseEntity{
 
     @Column(nullable = false)
     private String title;
+
     @Column(nullable = false)
     private Integer duration;
 
@@ -20,47 +27,4 @@ public class Song extends BaseEntity{
 
     @ManyToOne(fetch = FetchType.EAGER)
     private Style style;
-
-    public Song() {
-    }
-
-    public String getPerformer() {
-        return performer;
-    }
-
-    public void setPerformer(String performer) {
-        this.performer = performer;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public LocalDate getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(LocalDate releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public Integer getDuration() {
-        return duration;
-    }
-
-    public void setDuration(Integer duration) {
-        this.duration = duration;
-    }
-
-    public Style getStyle() {
-        return style;
-    }
-
-    public void setStyle(Style style) {
-        this.style = style;
-    }
 }
