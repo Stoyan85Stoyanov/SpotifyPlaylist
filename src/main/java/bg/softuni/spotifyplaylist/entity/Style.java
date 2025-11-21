7ffda474407e63b4c8811bd40ed2b713714e0171
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-21 19:36:15 +0200

    Spotify Playlist App

--- src/main/java/bg/softuni/spotifyplaylist/entity/Style.java
+++ src/main/java/bg/softuni/spotifyplaylist/entity/Style.java
@@ -1,8 +1,14 @@
 package bg.softuni.spotifyplaylist.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "styles")
 public class Style extends BaseEntity {
 
@@ -13,27 +19,9 @@ public class Style extends BaseEntity {
     @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
 
-    public Style() {
-    }
 
     public Style(StyleName name, String description) {
         this.styleName = name;
-        this.description = description;
-    }
-
-    public StyleName getStyleName() {
-        return styleName;
-    }
-
-    public void setStyleName(StyleName styleName) {
-        this.styleName = styleName;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
     }
 }
