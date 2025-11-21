b39b58966c77c84185905844e9dbfa8bbc0d1a69
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-21 19:36:38 +0200

    Spotify Playlist App

--- src/main/java/bg/softuni/spotifyplaylist/dto/UserRegisterDto.java
+++ src/main/java/bg/softuni/spotifyplaylist/dto/UserRegisterDto.java
@@ -3,7 +3,13 @@ package bg.softuni.spotifyplaylist.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserRegisterDto {
 
     @NotBlank
@@ -21,39 +27,4 @@ public class UserRegisterDto {
     @NotBlank
     @Size(min = 3, max = 20)
     private String confirmPassword;
-
-    public UserRegisterDto() {
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
 }
